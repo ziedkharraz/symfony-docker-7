be304efc005e5de5dd89cd6e67fe1e4ded0328f2
Zied KHARRAZ <zied.kharraz@gmail.com>
2025-04-02 22:21:35 +0200

    install twig & profiler

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('home/index.html.twig', ['number' => $number]);
+    }
+}
